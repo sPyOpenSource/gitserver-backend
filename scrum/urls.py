4e692894e857101de58064e7af7f0832981218bc
X. Wang <xuyi@tetra.nl>
2022-06-28 22:24:55 +0200

    Update urls.py

--- scrum/urls.py
+++ scrum/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import include, url
+from django.conf.urls import include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from supdem import views
 
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^api/', include('supdem.urls'))
+    re_path(r'^$', views.index, name='index'),
+    re_path(r'^api/', include('supdem.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
